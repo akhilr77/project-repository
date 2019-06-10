f1c893e6f8e7e330ef2c7ece650914eb0a274b6d
gsheng <gsheng112@gmail.com>
2019-06-10 18:53:29 -0400

    Runner and interface for printing quotient functionality created

--- Runner.java
+++ Runner.java
@@ -1,1 +1,12 @@
-
+public class Runner
+{
+	public Runner()
+	{
+		MethodInterfaceB func = (int x) -> {return Math.sqrt(x) % 1 == 0;};
+		System.out.printf("" + func.methodB(5) + "\n");
+	}
+	public static void main(String args[])
+	{
+		Runner app = new Runner();
+	}
+}
